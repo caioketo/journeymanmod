b8b6b577102b963f419340a6fad9761bb4754412
caioketo <caionmoreno1@gmail.com>
2012-10-15 18:52:13 -0300

    tools + make-jm script

---
+++ tools/make-jm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+    
+    echo "Select the android version of the build: "
+    echo ">1 - (4.1.2)"
+    echo " 2 - (4.1.1)"
+    read selected
+
+    ANDVER="4.1.2"
+
+    case $selected in
+    1) ANDVER='4.1.2' ;;
+    2) ANDVER='4.1.1' ;;
+    esac
+    
+    echo $andversion
+
+    DEFCONFIG=tuna_defconfig
+    ARCH="ARCH=arm"
+    DIR=..
+    ANDPATH='4.1.2'
+    RAMDIR=$DIR/tools/ramdisk
+    TOOLS=$DIR/tools
+    COMPILE="CCOMPILER=$TOOLS/arm-eabi-4.4.3/bin/arm-eabi-"
+    KERNEL=$DIR/$ANDVER/arch/arm/boot/zImage
+    
+    KBUILD_BUILD_VERSION="JourneymanMod-Kernel-alpha1"
+    cd ..
+    cd $ANDVER
+    export KBUILD_BUILD_VERSION
+    export $COMPILE
+    export $ARCH
+    make ARCH=arm $DEFCONFIG
+    make ARCH=arm CROSS_COMPILE=$CCOMPILER zImage
+    if [ -f $KERNEL ]; then
+	echo
+	echo "Kernel has been compiled ($ANDVER)!"
+	echo
+    else
+	echo
+	echo "Kernel did not compile, check for errors"
+	echo
+	exit
+    fi
+    cd ..
+    cd tools
+    $TOOLS/mkbootimg --kernel $KERNEL --ramdisk $RAMDIR/$ANDVER.gz --cmdline '' --base 80000000 --pagesize 2048 -o boot.img
+    
+    echo
+    echo "Boot.img created ok"
+    echo
